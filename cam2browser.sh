1741ee5fdafe5b34159ea201d1689d1327a193b7
NazimKenan <NazimKenan@users.noreply.github.com>
2016-09-01 13:22:23 +0200

    small efforts
    - added backup_this functionality for servers with creating non existing directories
    -web changed script's own filename

--- cam2browser.sh
+++ cam2browser.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 ### DESCRIPTION: Capturing images or videos from webcam and sending them to a webserver
 ### To do: video capturing, implenting motion or other tools; to be tested with other os than raspbian
-### please report bugs to http://github.com/NazimKenan/shellscripts/pygo2web.sh
+### please report bugs to http://github.com/NazimKenan/shellscripts/cam2browser.sh
+### for start on boot of pi add following line to your rc.local (sudo nano /etc/rc.local):
+### su pi -c '/home/pi/bin/cam2browser/cam2browser.sh 2> /home/pi/bin/cam2browser/error.log &'
 
-myName="pygo2web"
+myName="cam2browser"
 myVersion="0.1a"
 myIntention="Capturing images or videos from webcam and sending them to a webserver"
-myHome="http://github.com/NazimKenan/shellscripts/pygo2web.sh"
-myLocalPath="/home/pi/bin/$myName" #please provide explicit path to this sh-file's directory <-- very important!
+myHome="http://github.com/NazimKenan/shellscripts/cam2browser.sh"
+myLocalPath="/home/pi/bin/$myName" #please provide explicit path to this sh-file's
+#directory (not ~/.. or symbolic links) <-- very important!
 
 ################################################################################################
 ################################ FUNCTIONS #####################################################
@@ -19,30 +22,31 @@ function get_config() {
 		echo "Creating random configuration..."
 cat << EOF > $myLocalPath/$myName.config
 #!/bin/bash
-# Description: configuration file for pygo2web.sh - delete this file and pygo2web will generate
+# Description: configuration file for cam2browser.sh - delete this file and cam2browser will generate
 # new configuration file with next run
 
 #server credentials - use when streams are sent to another webserver - never leave on unsecured devices - can lead to security issues
 server="1" #switch to publish on different server - 1 for publishing, 0 for not publishing
-serverAddress="yourServer.com" # try uberspace.de if you're from Germany/Austria or Switzerland
-serverDirectory="/var/www/html/$myName" # change to your Document Root
-serverUser="yourUserName"
-serverPassword="yourPassword"
-serverBackup=0 #shall backups be made on server? 1 for yes, 0 for no
-serverBackupDir=""
+serverAddress="yourServer.com"
+serverDirectory="/var/www/html/$myName"
+serverUser="yourUsername"
+serverPassword="yourPassword goes here - please be aware of everyone able to read it when accessing your device"
+serverBackup="0" #shall backups be made on server? 1 for yes, 0 for no
+serverBackupDir="/var/www/virtual/nzm7591/html/$myName/backup"
 
 
 #own webserver credentials - use when streams are published on localhost webserver - never leave on unsecured devices - can lead to security issues
 localhost="1" #switch to publish on localhost - 1 for publishing, 0 for not publishing
 localhostDirectory="/var/www/html/$myName"
-localhostBackup=1 #shall backups be made on localhost? 1 for yes, 0 for no
-localhostBackupDir="$myLocalPath/backup"
+localhostBackup="0" #shall backups be made on localhost? 1 for yes, 0 for no
+localhostBackupDir="/var/www/html/$myName/backup"
 
 #characteristics of captures to make and webpage to create
 filename="image.jpeg"
 format="jpeg"
-resolution="1280x720" #depends on your camera; higher resolution may lead to performance issues
-loopDelayForCamera="1" #seconds to wait before starting next capture-loop
+#resolution="1280x720" #depends on your camera; higher resolution may lead to performance issues
+resolution="640" #depends on your camera; higher resolution may lead to performance issues
+loopDelayForCamera="3" #seconds to wait before starting next capture-loop
 webpageFile="index.html"
 EOF
 
@@ -52,7 +56,7 @@ fi
 	source $myLocalPath/$myName.config
 }
 
-#checking whether tools are installed and installs them
+#trying to update your operating system
 function update_upgrade() {
 	read -p "Do you want to update and upgrade your operating system now? Type yes: " yes
 	if [[ $yes = 'yes' ]] ; then
@@ -62,47 +66,58 @@ function update_upgrade() {
 	fi
 }
 
-function package_exists() {
-    dpkg -s $1 &> /dev/null
-}
-
+#installs missing packages if needed
 function package_install() {
+	function package_exists() {
+    	dpkg -s $1 &> /dev/null
+	}
 	if ! package_exists $1 ; then
-	echo "$1 could not be found on your system. Download and installation will begin now."
-	updateUpgrade
-	sudo apt-get install $1
-else
-	echo "$1 is already installed."
-fi
+		echo "$1 could not be found on your system. Download and installation will begin now."
+		updateUpgrade
+		sudo apt-get install $1
+	else
+		echo "$1 is already installed."
+	fi
 }
 
-function create_dir() {
-		if [ ! -d $1 ] ; then
-			mkdir $1
-		fi
-}
-
-#upload function
+#upload function for files for both localhost and server
 function upload_this() {
-	if [[ $server == "1" ]] ; then
-		# still to do: creating directory if non-existing
-		scp $1 $serverUser@$serverAddress:$serverDirectory/$(basename $1) &
-	fi
 	if [[ $localhost == "1" ]] ; then
-		create_dir $localhostDirectory
-		cp $1 $localhostDirectory/$(basename $1) &
+		create_dir $localhostDirectory localhost #only if directory doesn't exist
+		cp $1 $localhostDirectory/$(basename $1)
+	fi
+	if [[ $server == "1" ]] ; then
+		create_dir $serverDirectory server #only if directory doesn't exist
+		scp $1 $serverUser@$serverAddress:$serverDirectory/$(basename $1)
 	fi
 }
 
-#to create backups on localhost or different server
+#to create backups both on localhost or a different server
 function backup_this() {
 	if [[ $localhostBackup == "1" ]] ; then 
-		create_dir $localhostBackupDir
+		create_dir $localhostBackupDir localhost
 		cp $1 $localhostBackupDir/$(date +"%Y-%m-%d-%H:%M:%S")-$(basename $1)
 	fi
-	if [[ $serverBackup == "1" ]] ; then 
-		# still to do: creating directory if non-existing
-		echo "Server backup does not work yet. Please support: $myHome"
+	if [[ $serverBackup == "1" ]] ; then
+		create_dir $serverBackupDir server
+		scp $1 $serverUser@$serverAddress:$serverBackupDir/$(date +"%Y-%m-%d-%H:%M:%S")-$(basename $1)
+	fi
+}
+
+#creating missing directories on localhost and server
+directory_checked="0"
+function create_dir() {
+	if [ $directory_checked != "1" ] ; then
+		echo "Checking directories for the first time."
+		if [ $2 == "localhost" ] && [ ! -d $1 ] ; then
+			mkdir $1
+		fi
+		if [ $2 == "server" ] && ( ssh $serverUser@$serverAddress "[ ! -d $1 ]" ) ; then
+			ssh $serverUser@$serverAddress "mkdir $1"
+		fi
+		directory_checked="1"
+	else
+		echo "Directories were checked already." #debug
 	fi
 }
 
@@ -160,7 +175,7 @@ window.onload = function() {
 <script src=https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js></script>
 <script src=js/bootstrap.min.js></script>
 <!--
-	FREEDOM FOR EDWARD SNOWDEN.
+	EAT THIS NSA: FREEDOM FOR EDWARD SNOWDEN!
 -->
 <html>
 EOF
